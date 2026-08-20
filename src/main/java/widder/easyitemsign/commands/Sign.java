e41829c5505f1306d027b1ec12dfc45476693083
Widder <felixschmelzer4@gmail.com>
2026-08-20 12:52:03 +0200

    Add a missing ";"

--- src/main/java/widder/easyitemsign/commands/Sign.java
+++ src/main/java/widder/easyitemsign/commands/Sign.java
@@ -108,7 +108,7 @@ public class Sign {
                 String name = savedTag.getStringOr("easyitemsign_owner","");
                 if (name.equals(source.getPlayer().getName().getString())) {
                     source.sendFailure(Component.literal("The item is already singed"));
-                    return false
+                    return false;
                 }else {
                     source.sendFailure(Component.literal("The item is already singed"));
                     return false;
